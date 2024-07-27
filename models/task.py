71b5b4d93a2fe1a19d213b794c8c0de3c970f320
Danil <dania.tumanov@gmail.com>
2024-07-27 16:18:46 +0300

    Add task model

---
+++ models/task.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+import enum
+
+from sqlalchemy.orm import mapped_column, Mapped, relationship
+from sqlalchemy import String, Enum, DateTime, ForeignKey
+
+from models.base import Base
+
+
+class TaskStatus(enum.Enum):
+    TO_DO = "to do"
+    ON_HOLD = "on hold"
+    IN_PROGRESS = "in progress"
+    COMPLETED = "completed"
+
+
+class TaskPriority(enum.Enum):
+    LOW = "low"
+    MEDIUM = "medium"
+    HIGH = "high"
+
+
+class Task(Base):
+    __tablename__ = 'tasks'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str]
+    description: Mapped[str] = mapped_column(String, nullable=True)
+    status: Mapped[TaskStatus] = mapped_column(Enum(TaskStatus), default=TaskStatus.TO_DO)
+    priority: Mapped[TaskPriority] = mapped_column(Enum(TaskPriority), nullable=True)
+    owner_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    due_date: Mapped[datetime] = mapped_column(DateTime, nullable=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
+    updated_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now, onupdate=datetime.now)
+
+    owner: Mapped["User"] = relationship(back_populates="tasks")
