4c04e243eb63a816a422bcee720fd502e9950369
Danil <dania.tumanov@gmail.com>
2024-07-28 18:44:06 +0300

    Add tags rout

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter, FastAPI
 
-from routers import auth
+from routers import auth, tasks, tags
 
 api_router = APIRouter()
 api_router.include_router(auth.router)
+api_router.include_router(tasks.router)
+api_router.include_router(tags.router)
 
 app = FastAPI()
 
