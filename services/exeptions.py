5ec32807e4c976d35f957ebf801ab8c1618961f5
Danil <dania.tumanov@gmail.com>
2024-07-28 18:42:04 +0300

    Add exceptions for tags

--- services/exeptions.py
+++ services/exeptions.py
@@ -4,3 +4,18 @@ class TaskNotFoundError(Exception):
         self.message = message
         super().__init__(self.message)
 
+
+class TagNotFoundError(Exception):
+    """Exception raised when a tag is not found."""
+
+    def __init__(self, message="Tag not found"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class TagAlreadyExistsError(Exception):
+    pass
+
+
+class TagNotAssociatedError(Exception):
+    pass
