bcc81ec12442afa7d2b6067c1331148b80524d3d
Danil <dania.tumanov@gmail.com>
2024-08-21 13:57:04 +0300

    Add get_user_task endpoint

--- routers/tasks.py
+++ routers/tasks.py
@@ -8,8 +8,8 @@ from sqlalchemy.exc import IntegrityError
 from core.database import get_db
 from core.auth import get_current_user
 from services.exeptions import TaskNotFoundError
-from services.tasks import get_user_tasks_list, create_task, update_task, delete_task
-from shemas.task import TaskCreate
+from services.tasks import get_user_tasks_list, create_task, update_task, delete_task, get_user_task_detail
+from shemas.task import TaskCreate, Task
 from shemas.user import User
 
 
@@ -22,17 +22,30 @@ def get_user_tasks(current_user: Annotated[User, Depends(get_current_user)], db:
     return tasks
 
 
+@router.get("/tasks/{task_id}/")
+def get_user_task(task_id: int,
+                  current_user: Annotated[User, Depends(get_current_user)],
+                  db: Session = Depends(get_db)) -> Task:
+    try:
+        task = get_user_task_detail(db, current_user.id, task_id)
+    except TaskNotFoundError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    return Task.from_orm(task)
+
+
 @router.post("/tasks/", status_code=status.HTTP_201_CREATED)
-def create_user_task(task_form: TaskCreate, current_user: Annotated[User, Depends(get_current_user)], db: Session = Depends(get_db)):
+def create_user_task(task_form: TaskCreate,
+                     current_user: Annotated[User, Depends(get_current_user)],
+                     db: Session = Depends(get_db)) -> Task:
     try:
         task = create_task(db, task_form, current_user.id)
     except IntegrityError as e:
         raise HTTPException(status_code=400, detail=e.statement)
-    return task
+    return Task.from_orm(task)
 
 
 @router.put("/tasks/{task_id}/", status_code=status.HTTP_201_CREATED)
-def update_user_task(task_id,
+def update_user_task(task_id: int,
                      task_form: TaskCreate,
                      current_user: Annotated[User, Depends(get_current_user)],
                      db: Session = Depends(get_db)) -> TaskCreate:
@@ -40,13 +53,13 @@ def update_user_task(task_id,
         task = update_task(db, task_form, task_id,  current_user.id)
     except TaskNotFoundError as e:
         raise HTTPException(status_code=400, detail=str(e))
-    return TaskCreate.from_orm(task)
+    return Task.from_orm(task)
 
 
 @router.delete("/tasks/{task_id}/", status_code=status.HTTP_204_NO_CONTENT)
-def delete_user_task(task_id,
+def delete_user_task(task_id: int,
                      current_user: Annotated[User, Depends(get_current_user)],
-                     db: Session = Depends(get_db)):
+                     db: Session = Depends(get_db)) :
     try:
         delete_task(db, task_id,  current_user.id)
     except TaskNotFoundError as e:
